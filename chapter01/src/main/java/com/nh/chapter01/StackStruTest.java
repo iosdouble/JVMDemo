dc61bc0ec8dd2706d929c418b5b9e9c50b03ecfe
nihui <18202504057@163.com>
2021-02-04 23:26:30 +0800

    初始化提交

---
+++ chapter01/src/main/java/com/nh/chapter01/StackStruTest.java
@@ -0,0 +1,20 @@
+package com.nh.chapter01;
+
+/**
+ * @Classname StackStruTest
+ * @Description TODO
+ * @Date 2021/2/4 11:17 PM
+ * @Created by nihui
+ * @Version 1.0
+ * @Description StackStruTest @see JVMDemo
+ */
+public class StackStruTest {
+    public static void main(String[] args) {
+
+        //int i = 2+3;
+        int i = 2;
+        int j = 3;
+        int k = i+j;
+
+    }
+}
